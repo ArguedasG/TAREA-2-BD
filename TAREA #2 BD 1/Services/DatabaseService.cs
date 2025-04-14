83eabd2588665d5c7428524388730b1a7a348147
ArguedasG <gaarguedas@estudiantec.cr>
2025-04-13 18:29:18 -0600

    Termino el archivo de DatabaseService por ahora

--- TAREA #2 BD 1/Services/DatabaseService.cs
+++ TAREA #2 BD 1/Services/DatabaseService.cs
@@ -29,5 +29,85 @@ namespace TAREA__2_BD_1.Services
                 }
             }
         }
+        public async Task<List<Empleado>> ListarEmpleadosAsync(string filtro = "")
+        {
+            var empleados = new List<Empleado>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand("ListarEmpleados", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Filtro", filtro);
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            empleados.Add(new Empleado
+                            {
+                                Id = reader.GetInt32("Id"),
+                                ValorDocumentoIdentidad = reader.GetString("ValorDocumentoIdentidad"),
+                                Nombre = reader.GetString("Nombre"),
+                                IdPuesto = reader.GetInt32("IdPuesto"),
+                                FechaContratacion = reader.GetDateTime("FechaContratacion"),
+                                SaldoVacaciones = reader.GetDecimal("SaldoVacaciones"),
+                                EsActivo = reader.GetBoolean("EsActivo")
+                            });
+                        }
+                    }
+                }
+            }
+            return empleados;
+        }
+
+        public async Task<int> InsertarEmpleadoAsync(Empleado empleado)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand("InsertarEmpleado", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@ValorDocumentoIdentidad", empleado.ValorDocumentoIdentidad);
+                    command.Parameters.AddWithValue("@Nombre", empleado.Nombre);
+                    command.Parameters.AddWithValue("@IdPuesto", empleado.IdPuesto);
+                    command.Parameters.AddWithValue("@FechaContratacion", empleado.FechaContratacion);
+                    command.Parameters.AddWithValue("@SaldoVacaciones", empleado.SaldoVacaciones);
+                    command.Parameters.AddWithValue("@EsActivo", empleado.EsActivo);
+                    var returnValue = new SqlParameter("@ReturnVal", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.ReturnValue
+                    };
+                    command.Parameters.Add(returnValue);
+                    await command.ExecuteNonQueryAsync();
+                    return (int)returnValue.Value; // Código de error (0 si es éxito)
+                }
+            }
+        }
+
+        public async Task<List<Puesto>> ObtenerPuestosAsync()
+        {
+            var puestos = new List<Puesto>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand("SELECT Id, Nombre, SalarioxHora FROM Puesto ORDER BY Nombre", connection))
+                {
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            puestos.Add(new Puesto
+                            {
+                                Id = reader.GetInt32("Id"),
+                                Nombre = reader.GetString("Nombre"),
+                                SalarioxHora = reader.GetDecimal("SalarioxHora")
+                            });
+                        }
+                    }
+                }
+            }
+            return puestos;
+        }
     }
 }
