090e1793a3679dc4f6e3b8164f6d1bd9019c45dd
ArguedasG <gaarguedas@estudiantec.cr>
2025-04-21 20:23:21 -0600

    Se añaden los modelos de Movimientos y DetalleMovimientos

---
+++ TAREA #2 BD 1/Models/DetalleMovimientos.cs
@@ -0,0 +1,10 @@
+﻿namespace TAREA__2_BD_1.Models
+{
+    public class DetalleMovimientos
+    {
+        public string ValorDocumentoIdentidad { get; set; }
+        public string NombreEmpleado { get; set; }
+        public decimal SaldoVacaciones { get; set; }
+        public List<Movimiento> Movimientos { get; set; } = new List<Movimiento>();
+    }
+}
---
+++ TAREA #2 BD 1/Models/Movimiento.cs
@@ -0,0 +1,13 @@
+﻿namespace TAREA__2_BD_1.Models
+{
+    public class Movimiento
+    {
+        public DateTime FechaMovimiento { get; set; }
+        public string NombreTipoMovimiento { get; set; }
+        public decimal Monto { get; set; }
+        public decimal NuevoSaldo { get; set; }
+        public string NombreUsuario { get; set; }
+        public string IP { get; set; }
+        public DateTime FechaHoraRegistro { get; set; }
+    }
+}
